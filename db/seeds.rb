257d61b57b19515c18b36153ee0beff626d95268
cameron brock <cameronb@usc.edu>
2020-11-11 17:57:06 +0100

    can list flast with corect format

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do
+    my_flat = Flat.new({ name: Faker::Name.name,
+    address: Faker::Address.street_address,
+    description: Faker::Company.catch_phrase,
+    price_per_night: rand(45..200),
+    number_of_guests: rand(1..7) })
+     my_flat.save
+    puts my_flat
+end
